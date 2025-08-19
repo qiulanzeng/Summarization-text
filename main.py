612c8321222270c0d0f05bf1ce9a4943cee3f260
qiulan <qiulan.zeng@gmail.com>
2025-08-19 16:57:19 -0400

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from src.pipeline.step1_data_download import DataDownloadPipeline
-from src.pipeline.step2_data_tokenization import DataTokenizationPipeline
-from src.pipeline.step3_model_training import ModelTrainingPipeline
-# from src.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
-# from src.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
-from src.logging import logger
+from src.conponents.data_download import DataDownload
+from src.conponents.data_tokenization import DataTokenization
+from src.conponents.model_training import ModelTrainer
 
+from src.logging import logger
+import json
+
+
+with open("config.json", "r") as f:
+    config = json.load(f)
 
 # STEP = "1. Data download"
 # try:
 #    logger.info(f">>>>>> stage {STEP} started <<<<<<") 
-#    data_download = DataDownloadPipeline()
+#    data_download = DataDownload(config)
 #    data_download.main()
 #    logger.info(f">>>>>> stage {STEP} completed <<<<<<\n\nx==========x")
 # except Exception as e:
@@ -19,7 +22,7 @@ from src.logging import logger
 # STEP = "2. Data tokenization"
 # try:
 #    logger.info(f">>>>>> stage {STEP} started <<<<<<") 
-#    data_tokenization = DataTokenizationPipeline()
+#    data_tokenization = DataTokenization(config)
 #    data_tokenization.main()
 #    logger.info(f">>>>>> stage {STEP} completed <<<<<<\n\nx==========x")
 # except Exception as e:
@@ -29,7 +32,7 @@ from src.logging import logger
 STEP = "3. Model training"
 try:
    logger.info(f">>>>>> stage {STEP} started <<<<<<") 
-   model_training = ModelTrainingPipeline()
+   model_training = ModelTrainer(config)
    model_training.main()
    logger.info(f">>>>>> stage {STEP} completed <<<<<<\n\nx==========x")
 except Exception as e:
